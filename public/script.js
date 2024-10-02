5f7cf219894d61f664af2724ec28b4bab36eca21
Alexander Bokedal <alexander.bokedal@gmail.com>
2024-10-02 20:34:45 +0200

    added-comments

--- public/script.js
+++ public/script.js
@@ -49,6 +49,12 @@ const monsters = [
   },
 ];
 
+const doneButton = document.getElementById("done-button");
+doneButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  renderMonsters();
+});
+
 const addMonsterToArray = () => {
   const monsterName = document.getElementById("monsterName").value;
   const monsterType = document.getElementById("monsterType").value;
@@ -65,11 +71,49 @@ const addMonsterToArray = () => {
     monsterEyes: monsterEyes,
     monsterTentacles: monsterTentacles,
   };
+
+  monsters.push(newMonster);
 };
 
 ///////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////
 //////  SLUT PÅ KOD FÖR ATT LÄGGA TILL MONSTER/////////
 //////  SÖKORD: addMonster                //////////
+///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+//////  KOD för att visa MONSTER             /////////
+//////  SÖKORD: renderMonsters               //////////
+///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+
+const renderMonsters = () => {
+  const monsterGallery = document.getElementById("monsterGallery");
+  monsterGallery.innerHTML = "";
+
+  monsters.forEach((monster) => {
+    const monsterGalleryHtml = `
+    <div class="addForm">
+      <div class="carInfo">
+        <h2 class="carTitle">${monster.name}</h2>
+        <p class="carYear">Year: ${monster.monsterType}</p>
+        <p class="carMilage">Milage: ${monster.monsterHorns} KM</p>
+        <p class="carPrice">Price: $${monster.monsterLegs}</p>
+        <p class="carColor">Color: ${monster.monsterEyes}</p>
+        
+      </div>
+      </div>
+    `;
+    monsterGallery.innerHTML += monsterGalleryHtml;
+  });
+  console.log(1);
+};
+
+///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+//////  SLUT PÅ KOD KOD för att visa MONSTER  /////////
+//////  SÖKORD: renderMonsters               //////////
 ///////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////

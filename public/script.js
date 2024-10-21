77f2448bb9fedbea7f0764b1d680aecf4c436220
greenwinther <dennisgw@live.se>
2024-10-21 11:50:58 +0200

    darkmodefixed

--- public/script.js
+++ public/script.js
@@ -1192,3 +1192,23 @@ backgroundMusic.volume = 0.5;
 window.addEventListener("load", () => {
   backgroundMusic.play();
 });
+
+let darkmode = localStorage.getItem("darkmode");
+const themeSwitch = document.querySelector("#theme-switch");
+
+const enableDarkmode = () => {
+  document.body.classList.add("darkmode");
+  localStorage.setItem("darkmode", "active");
+};
+
+const disableDarkmode = () => {
+  document.body.classList.remove("darkmode");
+  localStorage.setItem("darkmode", null);
+};
+
+if (darkmode === "active") enableDarkmode();
+
+themeSwitch.addEventListener("click", () => {
+  darkmode = localStorage.getItem("darkmode");
+  darkmode !== "active" ? enableDarkmode() : disableDarkmode();
+});

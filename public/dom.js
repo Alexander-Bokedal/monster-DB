44f63586171d4612404b9b388e185c82e0d6b543
Henrik Cederlöf <Henrik.Cederlof@hotmail.com>
2024-10-25 10:01:30 +0200

    Städat

--- public/dom.js
+++ public/dom.js
@@ -1,31 +1,23 @@
 export const saveButton = document.querySelector("#save-button");
-export const monsterNameShow = document.querySelector(".monster-name-main");
-export const monsterSliders = document.querySelector("#sliders");
-export const colorsToChooseFrom = document.querySelector("#colors-container");
 export const doneButton = document.querySelector("#done-button");
 export const testButton = document.querySelector("#test-button");
-export const monsterNameInputField = document.querySelector("#monster-name");
-export const checkNameLength = document.querySelector(".check-name-length");
-export const clearFilterButton = document.querySelector("#clear-filter-button");
-export const dietSelectFilter = document.querySelector(
-  ".monster-diet-select-filter"
-);
+export const monsterSliders = document.querySelector("#sliders");
+export const muteSwitch = document.querySelector("#mute-switch");
+export const searchInput = document.querySelector("#search-input");
+export const themeSwitch = document.querySelector("#theme-switch");
 export const monsterDiet = document.querySelector(".monster-diet-select");
-export const monsterDietFilter = document.querySelector(
-  ".monster-diet-select-filter"
-);
 export const monsterType = document.querySelector(".monster-type-select");
 export const monsterSize = document.querySelector(".monster-size-select");
 export const monsterDietIcon = document.querySelector(".monster-diet-icon");
+export const monsterNameShow = document.querySelector(".monster-name-main");
 export const monsterTypeIcon = document.querySelector(".monster-type-icon");
 export const monsterSizeIcon = document.querySelector(".monster-size-icon");
-export const searchInput = document.querySelector("#search-input");
+export const checkNameLength = document.querySelector(".check-name-length");
+export const monsterNameInputField = document.querySelector("#monster-name");
 export const monsterPreviewWindow = document.querySelector("#monster-image");
-export const changeMonsterLeftBtn = document.querySelector(
-  "#change-monster-left"
-);
-export const changeMonsterRightBtn = document.querySelector(
-  "#change-monster-right"
-);
-export const themeSwitch = document.querySelector("#theme-switch");
-export const muteSwitch = document.querySelector("#mute-switch");
+export const colorsToChooseFrom = document.querySelector("#colors-container");
+export const clearFilterButton = document.querySelector("#clear-filter-button");
+export const changeMonsterLeftBtn = document.querySelector("#change-monster-left");
+export const changeMonsterRightBtn = document.querySelector("#change-monster-right");
+export const dietSelectFilter = document.querySelector(".monster-diet-select-filter");
+export const monsterDietFilter = document.querySelector(".monster-diet-select-filter");
